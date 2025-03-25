26fc11020ade5fdf85387716e3be8643fd17942d
AloyBanerjee <aloy.banerjee30@gmail.com>
2025-03-25 09:23:04 +0530

    Youtube Related activity

--- Coding/GenAI/PoC/45-YoutubeDownloader/app.py
+++ Coding/GenAI/PoC/45-YoutubeDownloader/app.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import validators
 import subprocess
 import os
 import glob
-
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
 
 ### Common Function
 
@@ -33,6 +37,7 @@ def extract_audio_old():
     command = f'yt-dlp -x --audio-format mp3 -P f"{output_dir}/audio" "{video_url}"'
     output = run_command(command)
     st.success("Audio extracted!")
+
 def extract_audio():
     if not video_url:
         st.warning("Please enter a YouTube URL.")
@@ -63,7 +68,6 @@ def list_formats():
     command = f'yt-dlp -F "{video_url}"'
     output = run_command(command)
 
-
 def run_command(command):
     """Executes a shell command and streams the output in a command-line style."""
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
@@ -79,6 +83,45 @@ def run_command(command):
     process.wait()
     return output_text
 
+def summarize(website_url, llm):
+    prompt_template="""
+        Provide a summary of the following content in {language} within 300 words:
+        Content:{text} 
+        """
+    prompt=PromptTemplate(template=prompt_template,input_variables=["text","language"])
+    try:
+        with st.spinner(f"Summarization is in progres for {website_url}, please keep waiting..."):
+            ## Validate the given url
+            if not website_url.strip():
+                st.error("Please provide the information to get started")
+            elif not validators.url(website_url):
+                st.error("Please enter a valid Url. It can may be a YT video utl or website url")
+            ## loading the website or yt video data
+            if "youtube.com" in website_url:
+                #loader=YoutubeLoader.from_youtube_url(website_url,add_video_info=True)
+                loader = YoutubeLoader.from_youtube_url(
+                    "https://www.youtube.com/watch?v=QsYGlZkevEg", add_video_info=False
+                )
+            else:
+                loader=UnstructuredURLLoader(urls=[website_url],ssl_verify=False,
+                                            headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+            docs=loader.load()                
+            if not docs:
+                st.error("No content could be loaded from the URL.")
+                st.stop()
+            
+            ## Chain For Summarization
+            chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+            output_summary = chain.run(
+                input_documents=docs,  # Pass as Document objects
+                language="Hindi"       # Pass additional variable
+            )
+            #output_summary=chain.run({"text": text, "language": "Hindi"})
+
+            st.success(output_summary)
+    except Exception as e:
+        st.exception(f"Exception:{e}")
+
 
 st.set_page_config(
         page_title="Video Downloader - Companion for Music Lover", page_icon="H:\Interview Preparation\Coding\GenAI\Tryouts\45-YoutubeDownloader\music.webp", layout="wide"
@@ -91,10 +134,12 @@ with col1:
 with col2:
     st.title("Video Downloader - Companion for Music Lover", anchor=False)
 
-
+st.info("Download YouTube videos, playlists, and extract audio with ease! In case you need summary of the video,"
+" you can use the Summarization tab to get the summary of the video content."
+" You can also place any website url to get the summary of the content.")
 
 # Input URL
-video_url = st.text_input("Enter YouTube Video/Playlist URL:")
+video_url = st.text_input("Enter YouTube Video/Playlist/Website URL:")
 
 # Select download type
 download_type = st.radio("Select Download Type:", ["Single Video", "Playlist"])
@@ -106,15 +151,24 @@ output_dir = st.text_input("Enter Download Location (Default: downloads/):", "do
 video_quality = st.selectbox("Select Video Quality:", ["best", "worst", "1080p", "720p", "480p", "360p"])  
 
 # Buttons for operations
-col1, col2, col3, col4, col5 = st.tabs([
+col0, col1, col2, col3, col4, col5, col6 = st.tabs([
+    "⚙️ Configuration",
     "📥 Download", 
     "🎵 Extract Audio",
     "📄 Get Metadata",
     "📌 List Available Formats", 
-    "▶️ Play Videos"
+    "▶️ Play Videos",
+    "📝 Summarization"
 ])
 
-
+with col0:
+    groq_api_key = st.text_input("Enter your Groq API Key:", type="password")
+    if groq_api_key:
+        if groq_api_key.strip():
+            llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama3-70b-8192")
+            st.success("Groq API Key & large langugae model is successfully set!")
+        else:
+            st.error("Please provide the api key to get started")
 with col1:
     if st.button("📥 Download Video", type='primary'):
         download()
@@ -134,3 +188,8 @@ with col5:
         selected_video = st.selectbox("🎬 Select a video to play:", st.session_state["downloaded_files"])
         if selected_video:
             st.video(selected_video, )
+with col6:
+    st.title("🦜 Summarize Text From YT or Website")
+    if st.button("📝 Summerize Content", type='primary'):
+        #summarize(video_url, llm)
+        st.warning('This feature is under development. Please check back later.')
